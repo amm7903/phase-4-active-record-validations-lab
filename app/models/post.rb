9b0d75a30eeb0ab8a85dd28cc99c29efa7b78777
Alejandro Marcano <+>
2021-11-24 02:11:28 +0100

    commit tree 68a7917bd290a2476c976e29fea140a9d9fa7c84

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,29 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+    CLICKBAIT_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    
+      def clickbait?
+        if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+          errors.add(:title, "must be clickbait")
+        end
+    
+    
+    
+    
+    
+    # validate :check_title
+
+    # def check_title
+    #     validates :title, :inclusion => { :in => %w(Won't Believe Secret Top Guess),
+    #         :message => "The value: %{Top} is not included in Field Type." }
+    end
 end
